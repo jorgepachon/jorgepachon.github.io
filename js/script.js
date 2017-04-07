46dcfdd523526e61d1405869a41a63051f680e90
Jorge Pachon Sanchez <jorge.pachon@live.u-tad.com>
2017-04-08 00:22:39 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -25,11 +25,17 @@ function myFunction(arr) {
         var p3 = document.createElement("p");
         var p4 = document.createElement("p");
         var p5 = document.createElement("p");
+        var p6 = document.createElement("p");
+        var p7 = document.createElement("p");
+        
          
 		var textoP = document.createTextNode(arr[i].titulo);
         var textoP2 = document.createTextNode(arr[i].FechaRealizacion);
         var textoP3 = document.createTextNode(arr[i].MiembrosGrupo);
         var textoP4 = document.createTextNode(arr[i].AsignaturasRelacionadas);
+        var textoP5 = document.createTextNode(arr[i].Disponible);
+        var textoP6 = document.createTextNode(arr[i].Responsive);
+        var textoP7 = document.createTextNode(arr[i].visualizacionRecomendada);
          
          if(i == 0){
             a.setAttribute("class", "home");
@@ -54,18 +60,24 @@ function myFunction(arr) {
 		
 		ul.appendChild(li);
         li.appendChild(a);
-         a.appendChild(span);
+        a.appendChild(span);
          
-        span.appendChild(p);
+         span.appendChild(p);
          span.appendChild(p2);
          span.appendChild(p3);
          span.appendChild(p4);
+         span.appendChild(p5);
+         span.appendChild(p6);
+         span.appendChild(p7);
          
          
         p.appendChild(textoP);
         p2.appendChild(textoP2);
         p3.appendChild(textoP3);
-		p4.appendChild(textoP4);
+        p4.appendChild(textoP4);
+        p5.appendChild(textoP5);
+        p6.appendChild(textoP6);
+        p7.appendChild(textoP7);
     }
 			
  
